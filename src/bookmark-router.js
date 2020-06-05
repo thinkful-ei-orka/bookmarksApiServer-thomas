33149cb32013f338f9e35af074db457bb92cee09
Sherry McCarty <sherrydmccarty@gmail.com>
2020-06-05 11:16:49 -0400

    Routes set, not coded though

--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -3,3 +3,13 @@ const { v4: uuid } = require('uuid')
 
 const bookmarksRouter = express.Router()
 const { bookmarks } = require('./store')
+const bodyParser = express.json()
+
+bookmarksRouter
+  .route('/')
+  .get((req, res) => {})
+  .post(bodyParser, (req, res) => {})
+
+bookmarksRouter
+  .route('/:id')
+  .delete((req, res) => {})
